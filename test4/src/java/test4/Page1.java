abf3cb1e989d4d1ba184af8189836b9e23df879a
psaw <psaw.mora@gmail.com>
2012-02-04 18:59:18 +0530

    first commit

---
+++ test4/src/java/test4/Page1.java
@@ -0,0 +1,140 @@
+/*
+ * Page1.java
+ *
+ * Created on Sep 21, 2010, 10:46:23 AM
+ * Copyright Prabath
+ */
+package test4;
+
+import com.icesoft.faces.component.ext.HtmlCommandButton;
+import com.sun.rave.web.ui.appbase.AbstractPageBean;
+import javax.faces.FacesException;
+import javax.faces.event.ValueChangeEvent;
+
+/**
+ * <p>Page bean that corresponds to a similarly named JSP page.  This
+ * class contains component definitions (and initialization code) for
+ * all components that you have defined on this page, as well as
+ * lifecycle methods and event handlers where you may add behavior
+ * to respond to incoming events.</p>
+ */
+public class Page1 extends AbstractPageBean {
+    // <editor-fold defaultstate="collapsed" desc="Managed Component Definition">
+    private int __placeholder;
+    
+
+    /**
+     * <p>Automatically managed component initialization.  <strong>WARNING:</strong>
+     * This method is automatically generated, so any user-specified code inserted
+     * here is subject to being replaced.</p>
+     */
+    private void _init() throws Exception {
+    }
+    private SessionBean1 s1;
+
+    /**
+     * Get the value of s1
+     *
+     * @return the value of s1
+     */
+    public SessionBean1 getS1() {
+        return s1;
+    }
+
+    /**
+     * Set the value of s1
+     *
+     * @param s1 new value of s1
+     */
+    public void setS1(SessionBean1 s1) {
+        this.s1 = s1;
+    }
+    private HtmlCommandButton button1 = new HtmlCommandButton();
+
+    public HtmlCommandButton getButton1() {
+        return button1;
+    }
+
+    public void setButton1(HtmlCommandButton hcb) {
+        this.button1 = hcb;
+    }
+
+    // </editor-fold>
+
+    /**
+     * <p>Construct a new Page bean instance.</p>
+     */
+    public Page1() {
+    }
+
+    /**
+     * <p>Callback method that is called whenever a page is navigated to,
+     * either directly via a URL, or indirectly via page navigation.
+     * Customize this method to acquire resources that will be needed
+     * for event handlers and lifecycle methods, whether or not this
+     * page is performing post back processing.</p>
+     * 
+     * <p>Note that, if the current request is a postback, the property
+     * values of the components do <strong>not</strong> represent any
+     * values submitted with this request.  Instead, they represent the
+     * property values that were saved for this view when it was rendered.</p>
+     */
+    public void init() {
+        // Perform initializations inherited from our superclass
+        super.init();
+        // Perform application initialization that must complete
+        // *before* managed components are initialized
+        // TODO - add your own initialiation code here
+            
+        // <editor-fold defaultstate="collapsed" desc="Managed Component Initialization">
+        // Initialize automatically managed components
+        // *Note* - this logic should NOT be modified
+        try {
+            _init();
+        } catch (Exception e) {
+            log("Page1 Initialization Failure", e);
+            throw e instanceof FacesException ? (FacesException) e: new FacesException(e);
+        }
+        
+        // </editor-fold>
+        // Perform application initialization that must complete
+        // *after* managed components are initialized
+        // TODO - add your own initialization code here
+    }
+
+    /**
+     * <p>Callback method that is called after the component tree has been
+     * restored, but before any event processing takes place.  This method
+     * will <strong>only</strong> be called on a postback request that
+     * is processing a form submit.  Customize this method to allocate
+     * resources that will be required in your event handlers.</p>
+     */
+    public void preprocess() {
+    }
+
+    /**
+     * <p>Callback method that is called just before rendering takes place.
+     * This method will <strong>only</strong> be called for the page that
+     * will actually be rendered (and not, for example, on a page that
+     * handled a postback and then navigated to a different page).  Customize
+     * this method to allocate resources that will be required for rendering
+     * this page.</p>
+     */
+    public void prerender() {
+    }
+
+    /**
+     * <p>Callback method that is called after rendering is completed for
+     * this request, if <code>init()</code> was called (regardless of whether
+     * or not this was the page that was actually rendered).  Customize this
+     * method to release resources acquired in the <code>init()</code>,
+     * <code>preprocess()</code>, or <code>prerender()</code> methods (or
+     * acquired during execution of an event handler).</p>
+     */
+    public void destroy() {
+    }
+
+    public void inputTextarea1_processValueChange(ValueChangeEvent vce) {
+    }
+}
+
